1ae22a76be385b9d75c498b15992d66db5f30b53
Michael <michaelkmb@hotmail.com>
2022-10-10 15:56:21 -0300

    allProducts structure;

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function showHome() {
   $(".content").load("home.html");
+  changeAdmToLogin();
 }
 
 function showLogin() {
@@ -8,8 +9,21 @@ function showLogin() {
 
 function showAddProduct() {
   $(".content").load("addProduct.html");
+  changeLoginToAdm();
 }
 
 function showViewProducts() {
   $(".content").load("viewProducts.html");
+}
+
+function changeLoginToAdm() {
+  const loginBtn = document.querySelector(".login__btn");
+  loginBtn.textContent = "Menu Administrador";
+  loginBtn.onclick = showViewProducts;
+}
+
+function changeAdmToLogin() {
+  const loginBtn = document.querySelector(".login__btn");
+  loginBtn.textContent = "Menu Administrador";
+  loginBtn.onclick = showViewProducts;
 }
